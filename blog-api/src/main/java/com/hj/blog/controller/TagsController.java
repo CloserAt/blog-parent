0dbdd1f3a7b91f6ca4fa60c08f0dfc7b3ea18dd9
Closer <13388031805@163.com>
2022-06-25 14:21:54 +0800

    6.25_first_commit

--- blog-api/src/main/java/com/hj/blog/controller/TagsController.java
+++ blog-api/src/main/java/com/hj/blog/controller/TagsController.java
@@ -3,6 +3,29 @@ package com.hj.blog.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+接口url：/tags/hot
+
+请求方式：GET
+
+请求参数：无
+
+返回数据：
+
+~~~json
+{
+    "success": true,
+    "code": 200,
+    "msg": "success",
+    "data": [
+        {
+            "id":1,
+            "tagName":"4444"
+        }
+    ]
+}
+~~~
+ */
 @RestController
 @RequestMapping("/tags/hot")
 public class TagsController {
