714e4b7babc5f0da69a0593cdb52861efab3367d
Closer <13388031805@163.com>
2022-08-14 23:06:35 +0800

    2022.08.14

--- blog-api/src/main/java/com/hj/blog/BlogApp.java
+++ blog-api/src/main/java/com/hj/blog/BlogApp.java
@@ -3,9 +3,12 @@ package com.hj.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class BlogApp {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(BlogApp.class,args);
     }
 }
--- blog-api/src/main/java/com/hj/blog/admin/config/WebMvcConfig.java
+++ blog-api/src/main/java/com/hj/blog/admin/config/WebMvcConfig.java
@@ -17,10 +17,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         //跨域配置(8080访问8888也会有跨域问题):不可以为*，不安全。前后端分离项目，可能域名不一致
         //本地测试 端口不一致也算跨域
-        registry.addMapping("/**").allowedOrigins("http://localhost:8080");//允许8080端口访问所有端口
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("POST","GET","PUT","OPTIONS","DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);//允许8080端口访问所有端口
     }
-
-
-
-
 }
